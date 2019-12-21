1caa6862f65514d69b3f9ff8044ae78146ca79e5
memmojtaba <mem.mojtaba@gmail.com>
2019-12-21 23:59:38 +0330

    web server

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 8080;
+const host = process.env.EFFECTIVE_ADD || 'localhost';
+
+const server = http.createServer(app);
+
+server.listen(port, host, () => {
+    console.log(`Server is running at ${host}:${port}`);
+});
