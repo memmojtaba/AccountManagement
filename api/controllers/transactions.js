a0ac862c4a07481c830952a985dd8b8664aade9a
memmojtaba <mem.mojtaba@gmail.com>
2019-12-22 13:12:14 +0330

    Getting data from the database with token added.

--- api/controllers/transactions.js
+++ api/controllers/transactions.js
@@ -1,15 +1,34 @@
 const mongoose = require('mongoose');
 
 const Transaction = require('../models/transaction');
+const Profile = require('../models/profile');
 
 exports.transactions_get_transaction = (req, res, next) => {
-    Transaction.find()
-        .select('_id profileID createdAt modifiedAt amount orderID statusCode refID')
+    Profile.find({ 'email': req.email })
+        .select('_id')
         .exec()
         .then(doc => {
-            console.log('From database:' + doc);
+            console.log('Found From profile: ' + doc[0]);
             if (doc) {
-                res.status(200).json(doc)
+                Transaction.find({profileID: doc[0]._id})
+                    .select('_id profileID createdAt modifiedAt amount orderID statusCode refID')
+                    .exec()
+                    .then(transactions => {
+                        console.log('Found From transaction: ' + transactions);
+                        if (transactions){
+                            res.status(200).json(transactions);
+                        } else {
+                            res.status(404).json({
+                                message: 'Invalid parameters.'
+                            });
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({ 
+                            message: 'Internal server error' 
+                        });
+                    });
             } else {
                 res.status(404).json({
                     message: 'Invalid parameters.'
@@ -18,9 +37,8 @@ exports.transactions_get_transaction = (req, res, next) => {
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({
-                message: 'Internal server error'
+            res.status(500).json({ 
+                message: 'Internal server error' 
             });
-
         });
 }
