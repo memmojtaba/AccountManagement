4644c7f17cdf919c9ed834bc86e0480b4384fd5d
memmojtaba <mem.mojtaba@gmail.com>
2019-12-23 00:07:23 +0330

    Setting of environment variables for production

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const http = require('http');
 const app = require('./app');
+
 
 const port = process.env.PORT || 8080;
 const host = process.env.EFFECTIVE_ADD || 'localhost';
