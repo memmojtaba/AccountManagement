25b211081ba36d8c875a65fab5920544f17e4efa
memmojtaba <mem.mojtaba@gmail.com>
2019-12-24 14:18:55 +0330

    Authentication before heartbeat removed.

--- api/routes/heartbeat.js
+++ api/routes/heartbeat.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 const checkAuth = require('../middlewares/check-auth');
 
-router.get('/', checkAuth, (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Account Management is up and running...'
     });
