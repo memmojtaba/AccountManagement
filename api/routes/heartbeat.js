fc59b7be00ea375e71152712efdec669e51e9274
memmojtaba <mem.mojtaba@gmail.com>
2019-12-24 14:20:02 +0330

    Authentication before heartbeat removed.

--- api/routes/heartbeat.js
+++ api/routes/heartbeat.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const http = require('http');
-
-const checkAuth = require('../middlewares/check-auth');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
