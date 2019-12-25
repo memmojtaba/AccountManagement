6c51c2ef3317fddc016cd53eee2dca6846ae4ab5
memmojtaba <mem.mojtaba@gmail.com>
2019-12-25 11:18:26 +0330

    Zarinpal payment request middleware added.

---
+++ api/middlewares/payment-request.js
@@ -0,0 +1,36 @@
+module.exports = (req, res, next) => {
+    try {
+        var soap = require('soap');
+        const payment_url = process.env.PAYMENT_URL
+            || 'https://sandbox.zarinpal.com/pg/services/WebGate/wsdl';
+        const callback_url = 'http://' + (process.env.EFFECTIVE_ADDR || 'localhost') + ':' +
+        (process.env.PORT || '8080') + '/account/pay/callback/' + req.transactionID + '/';
+        var options = {
+            MerchantID: process.env.MERCHANT_ID || 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX',
+            Amount: '10000', //from Trade Management service (orderID)
+            Description: 'تراکنش آزمایشی', //from Trade Management service (orderID)
+            Email: req.email,
+            Mobile: '',
+            CallbackURL: callback_url
+        };
+        console.log(options)
+        soap.createClient(payment_url, (err, client) => {
+            client.PaymentRequest(options, (err, result) => {
+                var body = JSON.parse(JSON.stringify(result));
+                if (Number(body.Status) === 100) {
+                    req.paymentStatus = body.Status;
+                    req.paymentAuthority = body.Authority;
+                    console.log(body);
+                    next();
+                } else {
+                    res.status(Number(body.Status)).json(body);
+                }
+            });
+        });
+
+    } catch {
+        res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+}
