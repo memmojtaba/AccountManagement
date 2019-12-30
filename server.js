28ab775da19ca5bc9548ad504a7752337e1aac97
memmojtaba <mem.mojtaba@gmail.com>
2019-12-30 12:23:31 +0330

    Minor edit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
