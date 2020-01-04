eb0057acbcca005451910b335e078feeb39f1746
memmojtaba <mem.mojtaba@gmail.com>
2020-01-04 13:13:14 +0330

    Token added to register API and Profile route modified.

--- api/routes/profiles.js
+++ api/routes/profiles.js
@@ -8,7 +8,7 @@ const register = require('../middlewares/register');
 const login = require('../middlewares/login');
 const getRole = require('../middlewares/get-role');
 
-router.post('/', register, login, ProfilesController.profiles_create_profile);
+router.post('/', register, ProfilesController.profiles_create_profile);
 router.put('/', getRole, ProfilesController.profiles_update_profile);
 router.get('/', getRole, ProfilesController.profiles_get_profile);
 
