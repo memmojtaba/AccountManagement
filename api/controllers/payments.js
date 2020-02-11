5bf08c19dce6a9e07bccbfe24bb9945fea3d8014
memmojtaba <mem.mojtaba@gmail.com>
2020-02-11 11:21:09 +0330

    Redirect to user interface after payment callback.

--- api/controllers/payments.js
+++ api/controllers/payments.js
@@ -13,7 +13,8 @@ exports.payments_zarinpal_payment = (req, res, next) => {
 
 exports.payments_callback_payment = (req, res, next) => {
     try {
-        res.status(200).json();
+        // res.status(200).json();
+        res.redirect(process.env.CALLBACK_URL);
     } catch (err) {
         console.log(err);
         res.status(500).json({
